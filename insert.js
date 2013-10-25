33cc4016509571d538c57a73bfac986c5430f26f
Xavi <xavi.rmz@gmail.com>
2013-10-24 22:08:49 -0700

    Got rid of unneeded file.

--- insert.js
+++
@@ -1,7 +0,0 @@
-(function() {
-	var app = document.createElement("script");
-	app.src = chrome.extension.getURL("app.js");
-	app.async = true;
-	app.addEventListener("load", function() { app.parentNode.removeChild(app); });
-	document.body.appendChild(app);
-})();
