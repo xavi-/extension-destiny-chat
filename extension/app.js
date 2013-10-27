c3995536dd1eb39d3fdd1029fb8cf9e56cc1d2df
Xavi <xavi.rmz@gmail.com>
2013-10-27 03:53:59 -0700

    Slide formatting tweak.

--- extension/app.js
+++ extension/app.js
@@ -5,5 +5,6 @@
 	spinner.parentNode.removeChild(spinner);
 
 	document.getElementById("twitch_chat").innerHTML =
-		'<iframe style="border:none;width:100%;height:100%;" src="http://destiny.gg/embed/chat"/>';
+		'<iframe style="border:none;width:100%;height:100%;" src="http://destiny.gg/embed/chat"/>'
+	;
 })(window, document);
